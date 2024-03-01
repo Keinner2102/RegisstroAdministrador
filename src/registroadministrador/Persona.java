94bc7ef7c68428fc66baf2bec3d78c28a7230825
unknown <SENA@GRDINSCTDGSD549.FormacionSena.RED>
2024-03-01 10:21:55 -0500

    Este es el registro de administradores

---
+++ src/registroadministrador/Persona.java
@@ -0,0 +1,96 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package registroadministrador;
+
+public class Persona {
+    public String nombre;
+    public String apellido;
+    public String FechaNacimiento;
+    public String telefono;
+    public String TipoDocumento;
+    public int NumDocumento;
+    public String correo;
+    public String direccion;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, String apellido, String FechaNacimiento, String telefono, String TipoDocumento, int NumDocumento, String correo, String direccion) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.FechaNacimiento = FechaNacimiento;
+        this.telefono = telefono;
+        this.TipoDocumento = TipoDocumento;
+        this.NumDocumento = NumDocumento;
+        this.correo = correo;
+        this.direccion = direccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getFechaNacimiento() {
+        return FechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String FechaNacimiento) {
+        this.FechaNacimiento = FechaNacimiento;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getTipoDocumento() {
+        return TipoDocumento;
+    }
+
+    public void setTipoDocumento(String TipoDocumento) {
+        this.TipoDocumento = TipoDocumento;
+    }
+
+    public int getNumDocumento() {
+        return NumDocumento;
+    }
+
+    public void setNumDocumento(int NumDocumento) {
+        this.NumDocumento = NumDocumento;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+    
+}
